0ed15f2efca49545da205650ba368af8d928ccd5
bikramgiri <asgbrg.github@gmail.com>
2025-07-30 20:34:19 +0545

    Fix error

--- obj/Debug/net9.0/TODO.AssemblyInfo.cs
+++ obj/Debug/net9.0/TODO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TODO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a38f3b682b146760d263ade651939cb7c29c67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83b4cf8ce77e48b9d855de8e335c0d35132d8e4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TODO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TODO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
